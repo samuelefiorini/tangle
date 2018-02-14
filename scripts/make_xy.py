bfab973959ad2f326bcb6e34583f7e034e8fcb7b
Samuele Fiorini <samuele.fiorini@gmail.com>
2018-02-14 19:23:25 +1100

    scripts/make_xy.py added

---
+++ scripts/make_xy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Script to generate a particular supervised dataset (X,y).
+
+Our goal here is to generate a the dataset D=(X, y) from the raw MBS-PBS 10%
+dataset. We will use the PBS dataset to identify which subjects after some point
+in time (in between 2008-2014) were prescribed to the use of some
+glycaemia-control drugs (see diabetes_drugs.csv).
+
+STEPS:
+1.
+"""
