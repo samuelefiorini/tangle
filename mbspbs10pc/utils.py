e74ce39fddde00709d904caa6104fcbd7c4901cf
Samuele Fiorini <samuele.fiorini@gmail.com>
2018-02-15 11:29:12 +1100

    make_xy arguments and input check

--- mbspbs10pc/utils.py
+++ mbspbs10pc/utils.py
@@ -1,11 +1,38 @@
 """Utility functions to load/visualize MBS-PBS 10% dataset."""
 
 from __future__ import division
-
+import os
 from collections import Counter
+import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+
+def check_input(root):
+    """Check the input dataset."""
+    yrange = range(2008, 2015)
+    mbs_files = ['MBS_SAMPLE_10PCT_'+str(year)+'.csv' for year in yrange]
+    pbs_files = ['PBS_SAMPLE_10PCT_'+str(year)+'.csv' for year in yrange]
+    sample_pin_lookout = 'SAMPLE_PIN_LOOKUP.csv'
+
+    print('Checking input...')
+    # Check for the SAMPLE_PIN_LOOKUP.csv file (not mandatory)
+    if not os.path.exists(os.path.join(root, sample_pin_lookout)):
+        warnings.warn('File {} not found in folder {}'.format(sample_pin_lookout, root))
+
+    # Check for the MBS files
+    for mbs in mbs_files:
+        if not os.path.exists(os.path.join(root, mbs)):
+            raise IOError('File {} not found in folder {}'.format(mbs, root))
+
+    # Check for the PBS files
+    for pbs in pbs_files:
+        if not os.path.exists(os.path.join(root, pbs)):
+            raise IOError('File {} not found in folder {}'.format(pbs, root))
+    print('ok.')
+
+
 
 def show_most_frequent(x, top_k=25, dpi=100, column=None, **kwargs):
     """Show the most frequent elements in a bar chart.
--- scripts/make_xy.py
+++ scripts/make_xy.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python
-"""Script to generate a particular supervised dataset (X,y).
+"""Script to generate the supervised dataset D = (X, y).
 
-Our goal here is to generate a the dataset D=(X, y) from the raw MBS-PBS 10%
+Our goal here is to generate a the dataset D = (X, y) from the raw MBS-PBS 10%
 dataset. We will use the PBS dataset to identify which subjects after some point
 in time (in between 2008-2014) were prescribed to the use of some
-glycaemia-control drugs (see diabetes_drugs.csv).
+glycaemia-control drugs (see ../data/drugs_used_in_diabetes.csv).
+These individuals will be labeled as our positive class (y = 1).
 
 STEPS:
 1.
 """
+
+import argparse
+import os
+
+from mbspbs10pc.utils import check_input
+
+
+def parse_arguments():
+    """"Parse input arguments."""
+    parser = argparse.ArgumentParser(description='MBS-PBS 10% data/labels '
+                                                 'extraction.')
+    parser.add_argument('-f', '--from_year', type=int,
+                        help='Diabetes drug starting year.', default=2012)
+    parser.add_argument('-r', '--root', type=str,
+                        help='Dataset root folder (default=../../data).',
+                        default=None)
+    parser.add_argument('-s', '--skip_input_check', action='store_false',
+                        help='Skipt the input check (default=True).')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """Main make_xy.py routine."""
+    args = parse_arguments()
+
+    # Check input dataset
+    if args.root is None:
+        ROOT = os.path.join('..', '..', 'data')
+    else:
+        ROOT = args.root
+    if not args.skip_input_check: check_input(ROOT)
+
+    # Check starting year
+    start_year = args.from_year
+    if start_year not in range(2008, 2015):
+        raise ValueError("Diabetes drug starting year must be in [2008-2014]")
+
+################################################################################
+
+if __name__ == '__main__':
+    main()
