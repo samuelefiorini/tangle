0ce8cf062004cf3c78cbb5ffa9bb10926e4c43d9
Samuele Fiorini <samuele.fiorini@gmail.com>
2018-02-27 11:45:01 +1100

    concessional threshold is now 0.75

--- mbspbs10pc/concessionals_utils.py
+++ mbspbs10pc/concessionals_utils.py
@@ -9,8 +9,8 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-__C0C1_THRESH__ = 0.5
-# __C0C1_THRESH__ = 0.75
+# __C0C1_THRESH__ = 0.5
+__C0C1_THRESH__ = 0.75
 
 
 def flatten(x):
