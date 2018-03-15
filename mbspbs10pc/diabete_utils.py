9b9bf931026e4bea779f34f4b3a88a9dfdb6153f
Samuele Fiorini <samuele.fiorini@gmail.com>
2018-03-15 16:20:23 +1100

    Repo cleaning

--- mbspbs10pc/diabete_utils.py
+++ mbspbs10pc/diabete_utils.py
@@ -146,161 +146,6 @@ def find_metonly(dd, pos_id, target_year=2012):
     return out
 
 
-def find_positive_samples(dd, cc, target_year=2012):
-    """Filter the population of interest according to the input target year.
-
-    This function returns the `'PTNT_ID'` of the subjects that started taking
-    diabetes drugs in the target year.
-
-    Parameters:
-    --------------
-    dd: dictionary
-        The output of find_diabetics().
-
-    cc: set
-        The intersection between the output of find_continuously_concessionals()
-        and find_consistently_concessionals().
-
-    target_year: integer (default=2012)
-        The target year
-
-    Returns:
-    --------------
-    positive_subjects: dict
-        Dictionary having target patient IDs (positive class) as keys and
-        SPPLY_DT as values.
-    """
-    _dd = dd['PBS_SAMPLE_10PCT_'+str(target_year)+'.csv']
-    # Init the postive subjects with the full list of people taking
-    # diabetes drugs in the target year
-    positive_subjects = set(_dd.keys())
-    positive_subjects = positive_subjects.intersection(cc)  # keep only the concessionals
-
-    for year in np.arange(2008, target_year)[::-1]:
-        curr = set(dd['PBS_SAMPLE_10PCT_'+str(year)+'.csv'].keys())
-        curr = curr.intersection(cc)  # keep only the concessionals
-        positive_subjects = set(filter(lambda x: x not in curr, positive_subjects))
-
-    # Retrieve the list of positive subjects
-    out = {}
-    for k in list(positive_subjects):
-        # (but change to the correct datetime format first)
-        out[k] = min(map(lambda x: pd.to_datetime(x, format='%d%b%Y'),
-                         _dd[k]['SPPLY_DT']))
-
-    return out
-
-
-def find_negative_samples(pbs_files, dd, cc):
-    """Find the negative samples PTNT_ID.
-
-    Negative samples are subjects that were NEVER prescribed to diabetes
-    controlling drugs.
-
-    Parameters:
-    --------------
-    pbs_files: list
-        List of input PBS filenames.
-
-    dd: dictionary
-        The output of find_diabetics().
-
-    cc: set
-        The intersection between the output of find_continuously_concessionals()
-        and find_consistently_concessionals().
-
-    Returns:
-    --------------
-    negative_subjects: list
-        The list of non target patient IDs (negative class).
-    """
-    # Start with an empty set, the iterate on each year and iteratively add
-    # to the negative subjects list, the patient id that are not selected
-    # as diabetic at the previous step.
-    negative_subjects = set()
-
-    diabetic_overall = set()
-    for pbs in pbs_files:  # get all the patients using diabetes drugs
-        diabetic_overall |= set(dd[os.path.split(pbs)[-1]])
-    diabetic_overall = diabetic_overall.intersection(cc)   # keep only the concessionals
-
-    for pbs in tqdm(pbs_files): # TODO maybe use multiprocessing here
-        curr = set(pd.read_csv(pbs, header=0, usecols=['PTNT_ID']).values.ravel())
-        curr = curr.intersection(cc)  # keep only the concessionals
-        # iteratively increase the set of indexes
-        negative_subjects |= set(filter(lambda x: x not in diabetic_overall, curr))
-
-    return list(negative_subjects)
-
-
-def find_diabetics(pbs_files, filter_copayments=False, n_jobs=1):
-    """Search people using diabetes drugs in input PBS files.
-
-    Parameters:
-    --------------
-    pbs_files: list
-        List of input PBS filenames.
-
-    filter_copayments: bool
-        When True, entries in the PBS data having
-        total cost < copayment(year) will be excluded. This improves
-        consistency for entries that are issued after April 2012. This only
-        holds for General Beneficiaries.
-
-    n_jobs: integer
-        The number of processes that have asyncronous access to the input file.
-
-    Returns:
-    --------------
-    out: dictionary
-        Dictionary of dictionaries as in the following example.
-        E.g.: `{'PBS_SAMPLE_10PCT_2012.csv': {3928691704: pd.DataFrame(), ...}}`
-        each DataFrame has
-    """
-    # Load the drugs used in diabetes list file
-    _dd = pd.read_csv(os.path.join(home[0], 'data', 'drugs_used_in_diabetes.csv'),
-                      header=0)
-
-    # Fix 6-digit notation
-    dd = set()  # dd should be a set for performance reasons
-    for item in _dd.values.ravel():
-        if len(item) < 6:
-            dd.add(str(0)+item)
-        else:
-            dd.add(item)
-
-    # Load the Co-payments thresholds
-    if filter_copayments:
-        co_payments = pd.read_csv(os.path.join(home[0], 'data', 'co-payments_08-18.csv'),
-                                  header=0, index_col=0, usecols=['DOC', 'GBC'])
-
-    # Load the PBS data in a global variable
-    # but keep only the rows relevant with diabete
-    global ___PBS_FILES_DICT__
-    for pbs in tqdm(pbs_files, desc='PBS files loading'):
-        pbs_dd = pd.read_csv(pbs, header=0, engine='c',
-                             usecols=['ITM_CD', 'PTNT_ID', 'SPPLY_DT',
-                                      'PTNT_CNTRBTN_AMT', 'BNFT_AMT'])
-        ___PBS_FILES_DICT__[pbs] = pbs_dd[pbs_dd['ITM_CD'].isin(dd)]
-
-    # Itereate on the pbs files and get the index of the individuals that
-    # were prescribed to diabes drugs
-    out = dict()
-    for pbs in tqdm(sorted(pbs_files), desc="PBS files processing"):
-        _pbs = os.path.split(pbs)[-1]  # more visually appealing
-
-        if filter_copayments:  # Select the appropriate co-payment threshold
-            year = int(_pbs.split('_')[-1].split('.')[0])
-            co_payment = co_payments.loc[year]['GBC']
-        else:
-            co_payment = None
-
-        out[_pbs] = find_diabetes_drugs_users(pbs, co_payment=co_payment,
-                                              n_jobs=n_jobs)
-
-    return out
-
-
 def find_diabetes_drugs_users(pbs, co_payment=None, n_jobs=1):
     """Find the diabetes drugs user from a single PBS file.
 
@@ -341,6 +186,64 @@ def find_diabetes_drugs_users(pbs, co_payment=None, n_jobs=1):
     jobs = [p.get() for p in jobs]
 
     return dict(results)
+
+
+def find_diabetics(pbs_files, n_jobs=1):
+    """Search people using diabetes drugs in input PBS files.
+
+    Parameters:
+    --------------
+    pbs_files: list
+        List of input PBS filenames.
+
+    n_jobs: integer
+        The number of processes that have asyncronous access to the input file.
+
+    Returns:
+    --------------
+    out: dictionary
+        Dictionary of dictionaries as in the following example.
+        E.g.: `{'PBS_SAMPLE_10PCT_2012.csv': {3928691704: pd.DataFrame(), ...}}`
+    +
+        each DataFrame has
+    """
+    # Load the drugs used in diabetes list file
+    _dd = pd.read_csv(os.path.join(home[0], 'data', 'drugs_used_in_diabetes.csv'),
+                      header=0)
+
+    # Fix 6-digit notation
+    dd = set()  # dd should be a set for performance reasons
+    for item in _dd.values.ravel():
+        if len(item) < 6:
+            dd.add(str(0)+item)
+        else:
+            dd.add(item)
+
+    # Load the PBS data in a global variable
+    # but keep only the rows relevant with diabete
+    global ___PBS_FILES_DICT__
+    for pbs in tqdm(pbs_files, desc='PBS files loading'):
+        pbs_dd = pd.read_csv(pbs, header=0, engine='c',
+                             usecols=['ITM_CD', 'PTNT_ID', 'SPPLY_DT',
+                                      'PTNT_CNTRBTN_AMT', 'BNFT_AMT'])
+        ___PBS_FILES_DICT__[pbs] = pbs_dd[pbs_dd['ITM_CD'].isin(dd)]
+
+    # Itereate on the pbs files and get the index of the individuals that
+    # were prescribed to diabes drugs
+    out = dict()
+    for pbs in tqdm(sorted(pbs_files), desc="PBS files processing"):
+        _pbs = os.path.split(pbs)[-1]  # more visually appealing
+
+        if filter_copayments:  # Select the appropriate co-payment threshold
+            year = int(_pbs.split('_')[-1].split('.')[0])
+            co_payment = co_payments.loc[year]['GBC']
+        else:
+            co_payment = None
+
+        out[_pbs] = find_diabetes_drugs_users(pbs, co_payment=co_payment,
+                                              n_jobs=n_jobs)
+
+    return out
 
 
 def worker(i, pbs, split, results, co_payment):
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 # Run main scripts
 
-# --- Assign labels --- #
-clear; python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2009
-python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2010
-python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2011
-python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2012
-python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2013
-python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2014
+# --- Get population of interest --- #
+# Find continuously and consistently concessionals on diabetes
+clear; python scripts/get_population_of_interest.py -r ../../data -o tmp/auxfile -m -nj 32
 
 # --- Extract raw data --- #
-clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2009_class_1.csv -nj 8
-clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_class_0.csv -nj 8
-# Negative examples
+# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2009_class_1.csv -nj 8
+# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_class_0.csv -nj 8
 
 # --- Prepare data for matching with CEM --- #
 # clear; python scripts/matching_step1.py -s tmp -o tmp/metformin
