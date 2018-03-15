dac829b61e2077d15861e37ce9c9c9197733bfc2
Samuele Fiorini <samuele.fiorini@gmail.com>
2018-03-15 14:33:29 +1100

    dummy

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Run main scripts
 
-# --- Make pkl.gs -- #
-# clear; python scripts/csv2pkl.py -r ../../data_lite -o tmp/lite
-
-
 # --- Assign labels --- #
-clear; python scripts/labels_assignment.py -r ../../data -o tmp/dump_lite -m -nj 32 -t 2009
+clear; python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2009
 python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2010
 python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2011
 python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2012
@@ -14,17 +10,34 @@ python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2013
 python scripts/labels_assignment.py -r ../../data -o tmp/dump -m -nj 32 -t 2014
 
 # --- Extract raw data --- #
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2009_class_1.csv -nj 3
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2010_class_1.csv -nj 3
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2011_class_1.csv -nj 3
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2012_class_1.csv -nj 3
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2013_class_1.csv -nj 3
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2014_class_1.csv -nj 3
-# clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_class_0.csv -nj 8 # Negative examples
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2009_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2009_METAFTER_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2009_METONLY_class_1.csv -nj 8
+
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2010_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2010_METAFTER_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2010_METONLY_class_1.csv -nj 8
+
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2011_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2011_METAFTER_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2011_METONLY_class_1.csv -nj 8
+
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2012_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2012_METAFTER_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2012_METONLY_class_1.csv -nj 8
+
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2013_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2013_METAFTER_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2013_METONLY_class_1.csv -nj 8
+
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2014_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2014_METAFTER_class_1.csv -nj 8
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_2014_METONLY_class_1.csv -nj 8
+
+clear; python scripts/extract_sequences.py -sic -r ../../data -ep -s tmp/dump_class_0.csv -nj 8 # Negative examples
 
 # --- Prepare data for matching with CEM --- #
-# clear; python scripts/matching_step1.py -s tmp -o tmp/dump
+clear; python scripts/matching_step1.py -s tmp -o tmp/metformin
 
-
-# --- Make {X,y} --- #
-# clear; Rscript scripts/matching_step2.R
+# --- Match with CEM --- #
+clear; Rscript scripts/matching_step2.R
